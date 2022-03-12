f7efce1ec3c6c9b971e44f9cd03ee721ce25d79a
Robyn Seymour-Jones <robynfsj@gmail.com>
2022-03-12 21:41:59 +0000

    add sql lesson 1

---
+++ foundation-week-1/sql-lesson-1/classwork-create-database-bakery.sql
@@ -0,0 +1,68 @@
+/*
+SQL LESSON 1—CREATING AND POPULATING A DATABASE
+-----------------------------------------------
+
+It is time to create our very first database and populate it with few tables.
+
+1. Create a database called Bakery.
+
+2. Let’s add two tables to the database. One should be called ‘Sweet’ and the one 
+should be called ‘Savoury’. 
+
+3. The Sweet table should have three columns: id (which 
+is a number), item_name (name of a pastry) and price (prices can be expressed in 
+pound and pennies).
+
+4. Include the same columns in the Savoury table. In addition to that add a column
+called main_ingredient (it will be a descriptive word) 
+
+5. Populate the database with the data provided.
+*/
+
+
+-- create database
+CREATE DATABASE nano_bakery;
+USE nano_bakery;
+
+-- create tables
+CREATE TABLE sweet(
+    id INT NOT NULL,
+    item_name VARCHAR(50),
+    price FLOAT(2)
+);
+
+CREATE TABLE savoury(
+    id INT NOT NULL,
+    item_name VARCHAR(50),
+    price FLOAT(2),
+    main_ingredient VARCHAR(100)
+);
+
+-- populate tables
+INSERT INTO sweet
+(id, item_name, price)
+VALUES
+(1, 'doughnut', 0.5),
+(2, 'croissant', 0.75),
+(3, 'painauchocolat', 0.55),
+(4, 'cinnamon twirl', 0.45),
+(5, 'cannoli', 0.88),
+(6, 'apple tart', 1.12);
+
+INSERT INTO savoury
+(id, item_name, price, main_ingredient)
+VALUES
+(1, 'meat pie', 1.25, 'pork'),
+(2, 'sausage roll', 1, null),
+(3, 'pasty', 2.45, 'beef');
+
+-- check tables
+SELECT * FROM sweet;
+SELECT * FROM savoury;
+
+
+
+
+    
+    
+
---
+++ foundation-week-1/sql-lesson-1/homework-create-database-parts.sql
@@ -0,0 +1,111 @@
+/*
+SQL LESSON 1—HOMEWORK
+-----------------------------------------------
+
+Create new Database called PARTS. The Database should contain four 
+tables, which must store the data provided.
+*/
+
+
+CREATE DATABASE nano_parts;
+USE nano_parts;
+
+
+-- 1. Create tables
+
+CREATE TABLE part(
+    p_id CHAR(2) NOT NULL,
+    pname VARCHAR(20),
+    colour VARCHAR(20),
+    weight INT,
+    city VARCHAR(20)
+);
+
+CREATE TABLE project(
+    j_id CHAR(2) NOT NULL,
+    jname VARCHAR(20),
+    city VARCHAR(20)
+);
+    
+CREATE TABLE supplier(
+    s_id CHAR(2) NOT NULL,
+    sname VARCHAR(20),
+    stat INT,
+    city VARCHAR(20)
+);
+
+CREATE TABLE supply(
+    s_id CHAR(2),
+    p_id CHAR(2),
+    j_id CHAR(2),
+    quantity INT
+);
+
+
+-- 2. Populate tables
+
+INSERT INTO part
+(p_id, pname, colour, weight, city)
+VALUES
+('P1', 'nut', 'red', 12, 'London'),
+('P2', 'bolt', 'green', 17, 'Paris'),
+('P3', 'screw', 'blue', 17, 'Rome'),
+('P4', 'screw', 'red', 14, 'London'),
+('P5', 'cam', 'blue', 12, 'Paris'),
+('P6', 'cog', 'red', 19, 'London');
+
+INSERT INTO project
+(j_id, jname, city)
+VALUES
+('J1', 'sorter', 'Paris'),
+('J2', 'display', 'Rome'),
+('J3', 'ocr', 'Athens'),
+('J4', 'console', 'Athens'),
+('J5', 'raid', 'London'),
+('J6', 'eds', 'Oslo'),
+('J7', 'tape', 'London');
+
+INSERT INTO supplier
+(s_id, sname, stat, city)
+VALUES
+('S1', 'Smith', 20, 'London'),
+('S2', 'Jones', 10, 'Paris'),
+('S3', 'Blake', 30, 'Paris'),
+('S4', 'Clark', 20, 'London'),
+('S5', 'Adams', 30, 'Athens');
+
+INSERT INTO supply
+(s_id, p_id, j_id, quantity)
+VALUES
+('S1', 'P1', 'J1', 200),
+('S1', 'P1', 'J4', 700),
+('S2', 'P3', 'J1', 400),
+('S2', 'P3', 'J2', 200),
+('S2', 'P3', 'J3', 200),
+('S2', 'P3', 'J4', 500),
+('S2', 'P3', 'J5', 600),
+('S2', 'P3', 'J6', 400),
+('S2', 'P3', 'J7', 800),
+('S2', 'P5', 'J2', 100),
+('S3', 'P3', 'J1', 200),
+('S3', 'P4', 'J2', 500),
+('S4', 'P6', 'J3', 300),
+('S4', 'P6', 'J7', 300),
+('S5', 'P2', 'J2', 200),
+('S5', 'P2', 'J4', 100),
+('S5', 'P5', 'J5', 500),
+('S5', 'P5', 'J7', 100),
+('S5', 'P6', 'J2', 200),
+('S5', 'P1', 'J4', 100),
+('S5', 'P3', 'J4', 200),
+('S5', 'P4', 'J4', 800),
+('S5', 'P5', 'J4', 400),
+('S5', 'P6', 'J4', 500);
+
+
+-- 3. Check tables
+
+SELECT * FROM part;
+SELECT * FROM project;
+SELECT * FROM supplier;
+SELECT * FROM supply;
